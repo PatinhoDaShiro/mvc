95ae10533ff297daff0dd371f93a33aedc81a72e
PatinhoDaShiro <81689830+PatinhoDaShiro@users.noreply.github.com>
2022-09-21 22:11:42 -0300

    Add files via upload

---
+++ mvc/app/controllers/HomeController.php
@@ -0,0 +1,52 @@
+<?php
+
+/**
+ * home - Controller de exemplo
+ * @author Cândido Farias
+ * @package mvc
+ * @since 0.1
+ */
+class HomeController extends MainController
+{
+
+	public function __construct(){
+		parent::__construct();
+		if(!isset($_SESSION['user'])){
+			header("Location:".URL_BASE."users/login");
+		}
+	}
+
+	/**
+	 * Carrega a página "/views/home/index.php"
+	 */
+    public function index() {
+		# Título da página
+		$this->title = 'Home';
+		$this->listar();
+		# Essa página não precisa de modelo (model)
+		
+		# Carrega os arquivos do view		
+		
+    } // index
+
+	public function listar($dateStart=null, $dateEnd=null) {
+		#Instanciar um objeto da classe MovimentModel 
+		$model=$this->load_model("Home");
+		//var_dump($model);
+		# busca a lista de movimento para o periodo
+		$listar=$model->listarInput($dateStart, $dateEnd);
+		$data['moviments']=$listar;
+		/*
+		$list=$model->listOut($dateStart, $dateEnd);
+		$batata['moviments'] = $list;
+		*/
+		//print_r($data);
+		/** Carrega os arquivos do view **/
+		$this->view->show('home/home', $data);
+		
+	}
+
+
+
+
+} // class HomeController
---
+++ mvc/app/models/HomeModel.php
@@ -0,0 +1,28 @@
+<?php
+
+class HomeModel extends MainModel{ 
+
+    public function listarInput($dateStart, $dateEnd){
+        $sql="SELECT DISTINCT date,(SELECT sum(value) FROM moviment WHERE date = m.date) as total, type from moviment m where type = 'input' ";
+
+		$retorno=$this->db->query($sql, null);
+		While($item=$retorno->fetch(PDO::FETCH_ASSOC)){
+			$resultado[]=$item;
+		}
+		return $resultado;
+    }
+	/*
+	public function listOut($dateStart, $dateEnd){
+		$sql="SELECT DISTINCT date,(SELECT sum(value) FROM moviment WHERE date = m.date) as total, type from moviment m where type = 'output' ";
+		$retorno=$this->db->query($sql, null);
+		While($item=$retorno->fetch(PDO::FETCH_ASSOC)){
+			$resultado[]=$item;
+
+		}
+		return $resultado;
+	}
+*/
+    
+
+
+}
